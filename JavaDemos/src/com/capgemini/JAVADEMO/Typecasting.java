01ef9f3734861ab16633d25464ff7a8e69d9325c
AKSHAY <AKSHAY@DESKTOP-K7A69U2>
2022-10-21 17:02:32 +0530

    first commit

---
+++ JavaDemos/src/com/capgemini/JAVADEMO/Typecasting.java
@@ -0,0 +1,17 @@
+package com.capgemini.JAVADEMO;
+
+public class Typecasting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+			
+		       float f = 10.532f;
+		       long l = (long) f;
+		       System.out.println(l);
+			
+
+
+	}
+
+}
